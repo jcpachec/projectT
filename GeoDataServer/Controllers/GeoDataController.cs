56d594f2b516c17c54443e0ce4163172c157db17
unknown <jcpachec@JCPACHEC-MOBL1.amr.corp.intel.com>
2015-05-21 18:22:20 -0500

    Initial

---
+++ GeoDataServer/Controllers/GeoDataController.cs
@@ -0,0 +1,53 @@
+﻿using GeoDataDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GeoDataServer.Controllers
+{
+    public class GeoDataController : ApiController
+    {
+
+        // GET api/geodata/5
+        public IEnumerable<GeoData> Get()
+        {
+
+            TGeoDataEntities dbContext = new TGeoDataEntities();
+
+            var locations = from k in dbContext.GeoDatas
+                            select k;
+
+            return locations.AsEnumerable();
+
+        }
+
+        // GET api/geodata/5
+        public IEnumerable<GeoData> Get(DateTime StartDate, DateTime EndDate)
+        {
+
+            using (TGeoDataEntities dbContext = new TGeoDataEntities())
+            {
+                var locations = from k in dbContext.GeoDatas
+                                where k.time == StartDate && k.time == EndDate
+                                select k;
+
+                return locations.AsEnumerable();
+            }
+        }
+
+        // POST api/geodata
+        public void Post(GeoData NewValue)
+        {
+            using (TGeoDataEntities dbContext = new TGeoDataEntities())
+            {
+                NewValue.time = DateTime.Now;
+                dbContext.GeoDatas.Add(NewValue);
+                dbContext.SaveChanges();
+            }
+        }
+      
+    }
+}
